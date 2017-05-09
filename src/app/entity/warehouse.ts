635f987eb4e704e39d29cb21066118dada60308d
Kirill Zyusko <zyusko.kirik@gmail.com>
2017-05-09 11:59:21 +0300

    add entity

---
+++ src/app/entity/warehouse.ts
@@ -0,0 +1,10 @@
+import {WarehouseCompany} from "./warehouseCompany";
+/**
+ * Created by Lenovo on 09.05.2017.
+ */
+
+export class Warehouse {
+  idWarehouse: number;
+  name: string;
+  warehouseCompany: WarehouseCompany;
+}
