954f8d163ec258242a997d494cf8dc65409b3937
colinforzeal <colinforzeal@yandex.ru>
2017-05-19 01:10:50 +0300

    changed customer create form to reactive

--- src/app/components/customer/customer-create/customer-create.component.ts
+++ src/app/components/customer/customer-create/customer-create.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {WarehouseCustomerCompany} from "../customer";
 import {WarehouseCustomerCompanyService} from "../customer.service";
 import {Router} from '@angular/router';
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-customer-create',
@@ -10,19 +11,32 @@ import {Router} from '@angular/router';
   providers: [WarehouseCustomerCompanyService]
 })
 export class CustomerCreateComponent implements OnInit {
+  customerForm: FormGroup;
   customer = new WarehouseCustomerCompany;
 
   constructor(private customerService: WarehouseCustomerCompanyService,
-              private router: Router) {
+              private router: Router,
+              private formBuiler: FormBuilder) {
+    this.customerForm = formBuiler.group({
+      'name': ['']
+    });
   }
 
   ngOnInit() {
   }
 
-  onSubmit(customer: WarehouseCustomerCompany) {
+  onSubmit(form: FormGroup) {
+    const customer = this.mapCustomerFromForm(form);
+    console.log(customer);
     this.customerService.save(customer).subscribe(success => {
       this.router.navigateByUrl('customer');
     });
+  }
+
+  private mapCustomerFromForm(form: FormGroup): WarehouseCustomerCompany {
+    const customer = new WarehouseCustomerCompany();
+    customer.name = form.controls['name'].value;
+    return customer;
   }
 
 }
