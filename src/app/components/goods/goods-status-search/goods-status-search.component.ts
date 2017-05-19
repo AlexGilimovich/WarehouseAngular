dfa6e674a884fd941c8888c3603631ddb0a93390
AGilimovich <a.gilimovich1990@gmail.com>
2017-05-19 17:46:32 +0300

    Added date validation in search form

--- src/app/components/goods/goods-status-search/goods-status-search.component.ts
+++ src/app/components/goods/goods-status-search/goods-status-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 import {GoodsStatusSearchDTO} from "../goodsStatusSearchDTO";
 import {GoodsStatusName} from "../goodsStatusName";
 import {statusMessages} from "../goods.module";
@@ -13,16 +13,31 @@ export class GoodsStatusSearchComponent implements OnInit {
   @Input() private status:GoodsStatusSearchDTO;
   @Input() private statusNames:GoodsStatusName[];
   private statusMessages = statusMessages;
+  private validationState = {from: true, to: true};
 
-  constructor(private searchService:SearchService) { }
+  constructor(private searchService:SearchService) {
+  }
 
   ngOnInit() {
   }
 
-  private removeStatus(){
+  private removeStatus() {
     this.searchService.removeGoodsSearchStatus(this.status);
   }
 
+  private validateFromDate(e) {
+    this.validationState.from = e.target.validity.valid;
+    this.generateValidationEvent();
+  }
+
+  private validateToDate(e) {
+    this.validationState.to = e.target.validity.valid;
+    this.generateValidationEvent();
+  }
+
+  private generateValidationEvent() {
+    this.searchService.dateValidationEvent(!this.validationState.from ? this.validationState.from : (!this.validationState.to ? this.validationState.to : true));
+  }
 
 
 }
