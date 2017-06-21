33a8fe853b0ffdea910874f4c098037c0ee081c2
student <student>
2017-06-21 16:56:08 +0300

    fixed warehouse space type issue

--- src/app/util/host.ts
+++ src/app/util/host.ts
@@ -2,7 +2,7 @@
  * Created by Lenovo on 09.05.2017.
  */
 export class Host {
-  static URL: string = "http://localhost:8081/web/web/";
+  static URL: string = "http://192.168.4.114:8081/web/web/";
 
   public static getURL(){
     return this.URL;
